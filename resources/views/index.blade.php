89611bf54b93ebcf772ea3966b65407891a88259
palkakapalka <kogan_daniel@inbox.lv>
2024-06-26 16:24:52 +0300

    add: main.css change: main.css, index.blade.ph

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,44 +1,58 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="index.style.css">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
+<div id="sidebar">
+    <h3>50 newest post</h3>
+    <p>1. Post title</p>
+    <p>#A #B #C</p>
+</div>
+
+<div id="main-content">
     @auth
-    <p>!!!!!!!!!!!!!!!!!!!!</p>
-    <h2>All Posts</h2>
-@foreach ($userPost as $post)
-<div style="border: 2px solid black;">
-    <h3>{{$post['title']}}</h3>
-    <p>{{$post['body']}}</p>
-</div>
-@endforeach
+        <h2>All Posts</h2>
+        @foreach ($userPost as $post)
+            <div class="post_list">
+                <h3>{{ $post['title'] }}</h3>
+                <p>{{ $post['body'] }}</p>
+            </div>
+        @endforeach
 
-    <form action="/logout" method="POST">
-        @csrf
-        <button>Log out</button>
-        <a href="/create_post">Create post</a><br><br>
-    </form>
+        <form action="/logout" method="POST">
+            @csrf
+            <button type="submit">Log out</button>
+        </form>
+        <a href="/create_post">Create post</a>
     @else
- <a href="/registration/">Registration</a><br><br>
- <a href="/login/">Login</a>
+        <div class="login">
+            <a href="/registration/">Registration</a>
+            <a href="/login/">Login</a>
+        </div>
 
- <h2>All Posts</h2>
-@foreach ($posts as $post)
-<div style="border: 2px solid black;">
-    <h3>{{$post['title']}}</h3>
-    <p>{{$post['body']}}</p>
+        <h2>All Posts</h2>
+        @foreach ($posts as $post)
+            <div class="post_list">
+                <h3>{{ $post['title'] }}</h3>
+                <p>{{ $post['body'] }}</p>
+            </div>
+        @endforeach
+    @endauth
 </div>
-@endforeach
- @endauth
 
-
-<div>
-
-
-
- </div>
+<div id="user-info">
+    @auth
+        <p class="username">{{ Auth::user()->name }}</p>
+        <p class="role">{{ Auth::user()->role }}</p>
+        <button>#-List</button>
+    @else
+        <a href="/registration/">Registration</a>
+        <a href="/login/">Login</a>
+    @endauth
+</div>
 </body>
 </html>

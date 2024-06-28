301c665105e4f1a49f420fb51ebac1a498a4ea5e
palkakapalka <kogan_daniel@inbox.lv>
2024-06-28 05:25:46 +0300

    switc from user posts to all posts

--- resources/views/view-post.blade.php
+++ resources/views/view-post.blade.php
@@ -9,25 +9,28 @@
 <h1>{{$post->title}}</h1>
 <p>{{$post->body}}</p>
 <div>
-@if($post->ImagePath)
-<p>Profile Image:</p>
-    <img src="{{asset('storage/' .$post->ImagePath) }}" alt="ffff" width="1000px">
-    @else
-        <p>No profile image available.</p>
-    @endif
-    <h2>Comments</h2>
-    @foreach($post->comments as $comments)
-        <p>{{ $comments->body }}</p>
-        <p>By: {{ $comments->user->name }}</p>
-    @endforeach
-    <form action="/create_commit/{{$post->id}}" method="POST">
-        @csrf
-            <label>Texts</label><br>
-            <textarea name="body" cols="35" rows="20"  placeholder="Texts......"></textarea><br>
-        </div>
-        <button type="submit">Save</button>
-    </form>
+    @if($post->ImagePath)
+    <p>Profile Image:</p>
+        <img src="{{asset('storage/' .$post->ImagePath) }}" alt="ffff" width="1000px">
+        @else
+            <p>No profile image available.</p>
+        @endif
+
+
+        <form action="/create_commit/{{$post->id}}" method="POST">
+            @csrf
+                <label>Comment Texts</label><br>
+                <textarea name="body" cols="35" rows="20"  placeholder="Texts......"></textarea><br>
+            <button type="submit">Save</button>   <a href="/">Back</a>
+        </form>
+        <h2>Comments</h2>
+        @foreach($post->comments as $comments)
+            <p>{{ $comments->user->name }}: {{ $comments->body }}</p>
+
+        @endforeach
+
 </div>
+
 
 
 
